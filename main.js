3a2fb40e858bd1f693c216e315f38dd54691891b
Santymrk2 <69694846+santymrk2@users.noreply.github.com>
2021-01-07 16:35:47 -0300

    main

--- main.js
+++ main.js
@@ -4,34 +4,32 @@ let UK = document.getElementById("UK");
 let send = document.getElementById("buttom");
 let cm = document.getElementById("medida");
 
-const chooseLoc = () => {
-if (USA===true) {
-    console.log("You chose USA");
-    return USA;
-} else if (EU===true){
-    console.log("You chose EU");
-    return EU;
-} else if (UK===true) {
-    console.log("You chose UK");
-    return UK;
-}
-};
-
-
-const medida = () => {
-    if (cm>22 | cm<29) {
-        console.log("Tu pie es de " + cm)
-    } else {
-        console.log("Debe ser un valor entre 22 y 29")
-    }
-};
 
 const enviar = () => {
     if (send===true) {
         chooseLoc();
-        medida();
+        chooseSize();
     }
 };
 
-enviar();
+const chooseLoc = () => {
+    if (USA===true) {
+        console.log("You chose USA");
+        return USA;
+    } else if (EU===true){
+        console.log("You chose EU");
+        return EU;
+    } else if (UK===true) {
+        console.log("You chose UK");
+        return UK;
+    }
+};
 
+
+const chooseSize = () => {
+    if (cm>= 22 | cm<= 29) {
+        console.log("Tu pie es de " + cm)
+    } else {
+          console.log("Debe ser un valor entre 22 y 29")
+    }
+};
