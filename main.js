7e9e37418d49f45080ba87f447460a44754d562a
Santymrk2 <69694846+santymrk2@users.noreply.github.com>
2021-01-07 23:23:55 -0300

    Anda

--- main.js
+++ main.js
@@ -1,34 +1,29 @@
 const chooseLoc = () => {
-    let USA = document.getElementById("USA");
-    let EU = document.getElementById("EU");
-    let UK = document.getElementById("UK");
+    let USA = document.getElementById("USA").checked;
+    let EU = document.getElementById("EU").checked;
+    let UK = document.getElementById("UK").checked;
     if (USA===true) {
         console.log("You chose USA");
-        return USA;
     } else if (EU===true){
         console.log("You chose EU");
-        return EU;
     } else if (UK===true) {
         console.log("You chose UK");
-        return UK;
     }
 };
 
+
 const chooseSize = () => {
-    let cm = document.getElementById("medida");
-    if (cm>= 22 | cm<= 29) {
-        console.log("Tu pie es de " + cm)
+    let cm = document.getElementById("size").value;
+    if (cm >= 22 && cm <= 29) {
+        console.log("Tu pie es de " + cm + " cm")
     } else {
-          console.log("Debe ser un valor entre 22 y 29")
+        console.log("Debe ser un valor entre 22 y 29")
     }
 };
 
 let enviar = () => {
-    let send = document.getElementById("buttom");
-    if (send===true) {
-        chooseLoc();
-        chooseSize();
-    }
+    chooseLoc();
+    chooseSize();
 };
 
-enviar();
+document.getElementById("buttom").addEventListener("click", enviar);
